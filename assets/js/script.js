62fd2b1ebb8ab38cb4fec40ea83f4b760eb0f1db
Jaxon Adams <jadamsresume1@gmail.com>
2022-02-12 00:02:30 -0700

    tasks now editable

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,26 @@ setInterval(function () {
     currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
     $("#currentDay").text(currentDate);
 }, 1000);
+
+// Edit event on click
+$(".timeblock").on("click", "p", function() {
+    var text = $(this).text().trim();
+
+    var textInput = $("<textarea>").val(text);
+
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+$(".timeblock").on("blur", "textarea", function() {
+    var text = $(this).text().trim();
+
+    var status = $(this).closest(".timeblock").attr("id");
+
+    events[status].text = text;
+    //saveEvents;
+
+    var eventP = $("<p>").addClass("event").text(text);
+
+    $(this).replaceWith(evenP);
+});
