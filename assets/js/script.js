2d975e5edd77ce90f309d3fde401c3a16a360b6c
Jaxon Adams <jadamsresume1@gmail.com>
2022-02-12 11:56:58 -0700

    background colors update based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,43 @@
 var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+var currentHour = moment();
+var timeblock = document.querySelector(".timeblock");
+var timeblockContainer = document.querySelector(".timeblock-container");
+
+// Set background color
+var setColor = function (element, color) {
+    element.style.backgroundColor = color;
+  }
+
+// Define auditEvent function
+var auditEvent = function(eventEl) {
+    var hourString = $(eventEl).find("span").text().trim();
+    // Change to 24hr Time (I know, this is inefficient)
+    if (hourString === "1:00") {
+        hourString = "13:00"
+    } else if (hourString === "2:00") {
+        hourString = "14:00"
+    } else if (hourString === "3:00") {
+        hourString = "15:00"
+    } else if (hourString === "4:00") {
+        hourString = "16:00"
+    } else if (hourString === "5:00") {
+        hourString = "17:00"
+    }
+    
+    var momentObj = moment(hourString, "HH:mm");
+
+    $(eventEl).removeClass("past present future");
+
+    console.log(currentHour, momentObj);
+
+    if (moment(currentHour, "hour").isSame(momentObj)) {
+        $(eventEl).addClass("present");
+    } else if (moment(currentHour, "hour").isAfter(momentObj)) {
+        $(eventEl).addClass("past");
+    } else if (moment(currentHour, "hour").isBefore(momentObj)) {
+        $(eventEl).addClass("future");
+    }
+}
 
 
 
@@ -33,3 +72,16 @@ $(".timeblock").on("blur", "textarea", function() {
 
     $(this).replaceWith(evenP);
 });
+
+// Check hour periodically
+$(".timeblock").each(function(index, el) {
+    auditEvent(el);
+    console.log(el);
+  });
+
+  setInterval(function() {
+    $(".timeblock").each(function(index, el) {
+        auditEvent(el);
+        console.log(el);
+      });
+  }, (1000 * 60) * 60);
